718ec365d7fa427a94dea9517a2ecfba62546596
Genesis Ian M. Palaganas <palaganas.genesis@gmail.com>
2025-06-25 00:53:37 +0800

    Rewrite GetAvailableLanguages to use go libraries instead of calling `find`
    The purpose of the original version was to learn how to do system calls
    with Go.

--- internal/ocr/languageutils/languageutils.go
+++ internal/ocr/languageutils/languageutils.go
@@ -3,40 +3,40 @@
 package languageutils
 
 import (
-	"os/exec"
+	"io/fs"
+	"os"
 	"slices"
 	"strings"
 )
 
+const (
+	trainedDataExt = ".traineddata"
+)
+
 // GetAvailableLanguages gets the available language for tesseract
 // from the provided tessdata dir path
-func GetAvailableLanguages(tessdataDir string) ([]string, error) {
-	// If the tessdataDir does not end with a slash, append one
-	// Exccept when empty string to avoid pointing to root directory
-	if tessdataDir != "" && tessdataDir[len(tessdataDir)-1] != '/' {
-		tessdataDir += "/"
-	}
+func GetAvailableLanguages(tessDataDir string) ([]string, error) {
+	languages := []string{}
 
-	cmdName := "find"
-	cmdArgs := []string{tessdataDir, "-maxdepth", "1", "-type", "f", "-name", "*.traineddata"}
-
-	cmdResult, err := exec.Command(cmdName, cmdArgs...).Output()
+	tessDataDirFS := os.DirFS(tessDataDir)
+	tessDataFiles, err := fs.Glob(tessDataDirFS, "*.traineddata")
 	if err != nil {
 		return nil, err
 	}
 
-	// Parses the output of the command to get the list of available languages
-	cmdResultStr := strings.ReplaceAll(string(cmdResult), tessdataDir, "")
-	cmdResultStr = strings.ReplaceAll(cmdResultStr, ".traineddata", "")
-	languages := strings.Split(strings.TrimSpace(cmdResultStr), "\n")
+	for _, tessDataFile := range tessDataFiles {
+		language := strings.Replace(tessDataFile, trainedDataExt, "", 1)
+		language = strings.Replace(language, tessDataDir, "", 1)
+		languages = append(languages, language)
+	}
 
 	return languages, nil
 }
 
 // CheckLanguageExists checks if the provided language exists in the
 // provided tessdata dir path
-func CheckLanguageExists(tessdataDir, language string) (bool, error) {
-	languages, err := GetAvailableLanguages(tessdataDir)
+func CheckLanguageExists(tessDataDir, language string) (bool, error) {
+	languages, err := GetAvailableLanguages(tessDataDir)
 
 	return slices.Contains(languages, language), err
 }
